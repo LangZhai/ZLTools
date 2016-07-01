83c549d67f70a7643aa1ad6e7dd303eb786a9c79
LangZhai <zl2012xyz@hotmail.com>
2016-07-01 15:10:53 +0800

    Daily update

--- dist/jquery.zltools.js
+++ dist/jquery.zltools.js
@@ -1,6 +1,6 @@
 /**
  * ZLTools
- * Date: 2016-06-16
+ * Date: 2016-07-01
  * © 2016 LangZhai(智能小菜菜)
  * This is licensed under the GNU LGPL, version 3 or later.
  * For details, see: http://www.gnu.org/licenses/lgpl.html
@@ -159,7 +159,7 @@ var params = eval({});
                     text = text.substring(0, text.length - 2);
                     $this.text(text + '…');
                 }
-            }).triggerHandler('resize.anchor' + id);
+            }).triggerHandler('resize.boxCut' + id);
         });
     };
 
